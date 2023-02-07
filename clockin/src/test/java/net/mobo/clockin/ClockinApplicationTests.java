1ff4fca85c208671f20a15a98d1b87a896aa27de
MoboGuo <GUOmb960113@163.com>
2023-02-07 15:34:40 +0800

    初次他提交

---
+++ clockin/src/test/java/net/mobo/clockin/ClockinApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mobo.clockin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
